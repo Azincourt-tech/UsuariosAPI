fe93f30a1c64fea0da7cb266020a41acb9499d41
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-03-29 15:25:38 -0300

    Criando a conexão com o banco e realizando as migrations

--- Database/UserDbContext.cs
+++ Database/UserDbContext.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace UsuariosAPI.Database
 {
     public class UserDbContext: IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
     {
-        public UserDbContext(DbContextOptions<UserDbContext> opt): base(opt)
-        {
 
+
+        public UserDbContext(DbContextOptions<UserDbContext> opt) : base(opt)
+        {
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)   // Reescreve metodo OnConfiguring, passando a string de conexão com o banco;
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json", false, true)
+                 .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("UsuarioConnection"));
+        }
+
+
+       
     }
 }
