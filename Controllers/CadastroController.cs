e38fd1b3b8192806edb59f3f687bd523595460aa
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-03-28 23:47:38 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using UsuariosAPI.Database.Dtos;
+
+namespace UsuariosAPI.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            return Ok(createDto);
+        }
+
+       
+    }
+}
